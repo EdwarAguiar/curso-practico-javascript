c53ec2fa1d396414c6101c02cab2daf6cb61c092
Edwar Aguiar <Aguiar.Edwar@gmail.com>
2021-09-12 22:37:44 -0500

    Taller 2 - Solucion 1: Arrays y switch

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ Curso Practico de Javascript de Platzi
 - Segundo paso: Implementar las fórmulas en JavaScript
 - Tercer paso: Crear funciones
 - Cuarto paso: integrar JS con HTML
+
+## Taller #2: Porcentajes y Descuentos
+
+- 1er Paso: Definir la formulas
+- 2do Paso: Implementar la formulas en JavaScript
+- 3er Paso: Crear Funciones
+- 4to Paso: Integrar Javascript con HTML
---
+++ css/descuentos.js
@@ -0,0 +1,45 @@
+const precioOriginal = 120;
+const descuento = 18;
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+const coupons = [
+    "Fall2021",
+    "NextLevel",
+    "Halloween",
+];
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+
+    let descuento;
+
+    switch(couponValue) {
+        case "Halloween":
+            descuento = 10;
+        break;
+        case "Fall2021":
+            descuento = 15;
+        break;
+        case "NextLevel":
+            descuento = 30;
+        break;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
+
+
+
